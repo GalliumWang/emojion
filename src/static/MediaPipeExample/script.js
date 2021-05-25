346a0d908b62cc8eebe89e3c1b8003273b006040
Gallium Wang <galliumwang199@gmail.com>
2021-05-26 01:26:24 +0800

    refactor(module): refactor mediapipe demo for hands detection

--- src/static/MediaPipeExample/script.js
+++ src/static/MediaPipeExample/script.js
@@ -1,3 +1,11 @@
+const COLOR_LIST = {
+  handsContour:'#b881e2',
+  handsJointContour: '#e8b06b',
+  handsJoint: '#7021ad'
+}
+
+let handsDetectionOn = false;
+
 // Our input frames will come from here.
 const videoElement =
     document.getElementsByClassName('input_video')[0];
@@ -11,36 +19,33 @@ const canvasCtx = canvasElement.getContext('2d');
 // call tick() each time the graph runs.
 const fpsControl = new FPS();
 
-// Optimization: Turn off animated spinner after its hiding animation is done.
-const spinner = document.querySelector('.loading');
-spinner.ontransitionend = () => {
-  spinner.style.display = 'none';
-};
+// TODO: optimize to hide loding logo dynamically
+const lodingComponent = document.querySelector('.loading');
 
 function onResults(results) {
-  // Hide the spinner.
-  document.body.classList.add('loaded');
-
   // Update the frame rate.
-  fpsControl.tick();
 
   // Draw the overlays.
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
   if (results.multiHandLandmarks && results.multiHandedness) {
     for (let index = 0; index < results.multiHandLandmarks.length; index++) {
       const classification = results.multiHandedness[index];
       const isRightHand = classification.label === 'Right';
       const landmarks = results.multiHandLandmarks[index];
+
+      // draw hands contour
       drawConnectors(
           canvasCtx, landmarks, HAND_CONNECTIONS,
-          {color: isRightHand ? '#00FF00' : '#FF0000'}),
+          {color: COLOR_LIST.handsContour}),
+
+      // draw hands joint
       drawLandmarks(canvasCtx, landmarks, {
-        color: isRightHand ? '#00FF00' : '#FF0000',
-        fillColor: isRightHand ? '#FF0000' : '#00FF00',
-        radius: (x) => {
-          return lerp(x.from.z, -0.15, .1, 10, 1);
-        }
+        color: COLOR_LIST.handsJointContour,
+        fillColor: COLOR_LIST.handsJoint,
+        radius: 1
       });
+
     }
   }
 }
@@ -48,14 +53,23 @@ function onResults(results) {
 const hands = new Hands({locateFile: (file) => {
   return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.1/${file}`;
 }});
+
+hands.setOptions({
+  maxNumHands: 2,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+})
 hands.onResults(onResults);
 
-/**
- * Instantiate a camera. We'll feed each frame we receive into the solution.
- */
 const camera = new Camera(videoElement, {
   onFrame: async () => {
-    await hands.send({image: videoElement});
+    fpsControl.tick();
+    if(lodingComponent.style.display !== "none"){
+      lodingComponent.style.display = 'none';
+    }
+    if(handsDetectionOn){
+      await hands.send({image: videoElement});
+    }
   },
   width: 1280,
   height: 720
@@ -66,30 +80,15 @@ camera.start();
 // options.
 new ControlPanel(controlsElement, {
       selfieMode: true,
-      maxNumHands: 2,
-      minDetectionConfidence: 0.5,
-      minTrackingConfidence: 0.5
+      handsDetectionOn: false
     })
     .add([
-      new StaticText({title: 'MediaPipe Hands'}),
+      new StaticText({title: '设置'}),
       fpsControl,
-      new Toggle({title: 'Selfie Mode', field: 'selfieMode'}),
-      new Slider(
-          {title: 'Max Number of Hands', field: 'maxNumHands', range: [1, 4], step: 1}),
-      new Slider({
-        title: 'Min Detection Confidence',
-        field: 'minDetectionConfidence',
-        range: [0, 1],
-        step: 0.01
-      }),
-      new Slider({
-        title: 'Min Tracking Confidence',
-        field: 'minTrackingConfidence',
-        range: [0, 1],
-        step: 0.01
-      }),
+      new Toggle({title: '镜像', field: 'selfieMode'}),
+      new Toggle({title: '手部识别', field: 'handsDetectionOn'})
     ])
     .on(options => {
+      handsDetectionOn = options.handsDetectionOn;
       videoElement.classList.toggle('selfie', options.selfieMode);
-      hands.setOptions(options);
     });
