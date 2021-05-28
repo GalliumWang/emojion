1065cbf3b0ea976b1f1a02092a7229e3f281994e
Gallium Wang <galliumwang199@gmail.com>
2021-05-28 12:04:22 +0800

    feat(backend): add json parse support for express

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 let facePlusPlus=require('./module/facePlusPlus');
 var express = require('express');
-const { response } = require('express');
+
 var app = express();
+app.use(express.urlencoded({extended: true, limit: '50mb'})); //TODO
+app.use(express.json({limit: '50mb'}))
 
 app.use('/', express.static('static'))
 
 app.post('/api/face-plus-plus-emotion-detect', function (req, res) {
-   // TODO
+   let imgBase64Data = req.body.base64Data;
+   facePlusPlus.facePlusPlusEmotionDetect(imgBase64Data).then(response=>{res.send(response)});
 })
 
 var server = app.listen(8080, function () {
